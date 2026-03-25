cc3ee7824aaf887f9b4403e74ef2ce0ea2a065c5
Dipesh Agrawal <harsh.agrawal123@gmail.com>
2026-03-25 19:10:25 +0530

    Setup backend server with Express

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express= require("express");
+
+const cors= require("cors")
+
+const app = express();
+
+//Middleware
+app.use(cors())
+
+app.use(express.json())
+
+//test route
+
+app.get("/",(req,res)=>{
+  res.send("file server is running ")
+})
+
+app.listen(5000,()=>{
+  console.log("server is running on port 5000")
+})
