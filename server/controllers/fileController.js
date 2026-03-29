6949f3715028bd8c91ac18569cb985c3e431e276
Dipesh Agrawal <harsh.agrawal123@gmail.com>
2026-03-29 09:16:57 +0530

    Add get files API to fetch user-specific files

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -1,5 +1,6 @@
 const File = require("../models/File")
 
+//upload file
 const uploadFile = async (req, res) => {
   try {
     const newFile = new File({
@@ -23,4 +24,25 @@ const uploadFile = async (req, res) => {
   }
 }
 
-module.exports = { uploadFile };
+
+//Get Files
+
+const getFiles = async(req,res)=>{
+  try{
+    const files = await File.find({userId:req.user.id})
+
+    res.status(200).json({
+     message: "file fetched successfuly",
+     files
+    })
+
+  }
+  catch(err){
+    console.error(err),
+    res.status(401).json({
+      message:"failed to fetch files"
+    })
+  }
+}
+
+module.exports = { uploadFile,getFiles };
--- server/routes/fileRoutes.js
+++ server/routes/fileRoutes.js
@@ -8,12 +8,14 @@ const upload = require("../config/multer")
 
 const authmiddleware = require("../middleware/authMiddleware");
 
-const {uploadFile} = require("../controllers/fileController")
+const {uploadFile,getFiles} = require("../controllers/fileController")
 
 
 //upload route
 
 router.post("/upload",authmiddleware,upload.single("file"),uploadFile);
+
+router.get("/",authmiddleware,getFiles)
 
 
 module.exports = router;
