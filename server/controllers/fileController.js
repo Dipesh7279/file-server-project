e51463b2e2f27f2c11bf81c8f5fb05d721a92efc
Dipesh Agrawal <harsh.agrawal123@gmail.com>
2026-03-29 08:48:35 +0530

    Add file model and save data in mongoDB

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -1,16 +1,26 @@
+const File = require("../models/File")
 
+const uploadFile = async (req, res) => {
+  try {
+    const newFile = new File({
+      filename: req.file.filename,
+      originalname: req.file.originalname,
+      path: req.file.path,
+      size: req.file.size,
+      userId: req.user.id
+    })
+    await newFile.save()
 
-const uploadFile=  (req,res)=>{
-  try{
     res.status(201).json({
-      message:"file uploaded successfully",
-      file: req.file })
-    }
-    catch(err){
-      res.status(500).json({
-        message:"upload failed"
-      })
-    }
+      message: "file uploaded successfully and saved to DB",
+      file: newFile
+    })
   }
+  catch (err) {
+    res.status(500).json({
+      message: "upload failed"
+    })
+  }
+}
 
 module.exports = { uploadFile };
